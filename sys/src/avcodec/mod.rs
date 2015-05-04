8731b391f31e48290aed3d0c012aa2937798d8b3
meh <meh@schizofreni.co>
2015-05-04 02:07:50 +0200

    avcodec: add bindings

---
+++ sys/src/avcodec/mod.rs
@@ -0,0 +1,37 @@
+mod codec;
+pub use self::codec::*;
+pub use self::codec::AVCodecID::*;
+pub use self::codec::Motion_Est_ID::*;
+pub use self::codec::AVDiscard::*;
+pub use self::codec::AVAudioServiceType::*;
+pub use self::codec::AVPacketSideDataType::*;
+pub use self::codec::AVFieldOrder::*;
+pub use self::codec::AVSubtitleType::*;
+pub use self::codec::AVPictureStructure::*;
+pub use self::codec::AVLockOp::*;
+
+mod fft;
+pub use self::fft::*;
+pub use self::fft::RDFTransformType::*;
+pub use self::fft::DCTTransformType::*;
+
+mod dv_profile;
+pub use self::dv_profile::*;
+
+mod dxva2;
+pub use self::dxva2::*;
+
+mod qsv;
+pub use self::qsv::*;
+
+mod vaapi;
+pub use self::vaapi::*;
+
+mod vda;
+pub use self::vda::*;
+
+mod vobis_parser;
+pub use self::vobis_parser::*;
+
+mod xvmc;
+pub use self::xvmc::*;
--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -1,8 +1,11 @@
 #![feature(libc)]
-#![allow(non_camel_case_types, raw_pointer_derive, non_snake_case)]
+#![allow(non_camel_case_types, raw_pointer_derive, non_snake_case, non_upper_case_globals)]
 
 extern crate libc;
 
 #[macro_use]
 mod avutil;
 pub use avutil::*;
+
+mod avcodec;
+pub use avcodec::*;
