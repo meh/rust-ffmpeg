c82cab4dbe11f9dfa98e37988a480af062cfb1bf
meh <meh@schizofreni.co>
2015-05-15 03:42:52 +0200

    cargo: add features for every library

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -7,17 +7,27 @@ extern crate libc;
 mod avutil;
 pub use avutil::*;
 
+#[cfg(feature = "avcodec")]
 mod avcodec;
+#[cfg(feature = "avcodec")]
 pub use avcodec::*;
 
+#[cfg(feature = "avdevice")]
 mod avdevice;
+#[cfg(feature = "avdevice")]
 pub use avdevice::*;
 
+#[cfg(feature = "avformat")]
 mod avformat;
+#[cfg(feature = "avformat")]
 pub use avformat::*;
 
+#[cfg(feature = "swscale")]
 mod swscale;
+#[cfg(feature = "swscale")]
 pub use swscale::*;
 
+#[cfg(feature = "postproc")]
 mod postproc;
+#[cfg(feature = "postproc")]
 pub use postproc::*;
