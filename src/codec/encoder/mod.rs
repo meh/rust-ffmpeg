cae8bb77d74a4e98cd48b9f29fc3034a099d6ee5
lummax <luogpg@googlemail.com>
2015-08-25 16:55:30 +0200

    codec: Improve codec::* struct state transitions (encoder)
    This commit improves the state transitions for the `codec::*` structs as
    discussed in https://github.com/meh/rust-ffmpeg/issues/7.

--- src/codec/encoder/mod.rs
+++ src/codec/encoder/mod.rs
@@ -1,11 +1,14 @@
 pub mod video;
 pub use self::video::Video;
+pub use self::video::Encoder as VideoEncoder;
 
 pub mod audio;
 pub use self::audio::Audio;
+pub use self::audio::Encoder as AudioEncoder;
 
 pub mod subtitle;
 pub use self::subtitle::Subtitle;
+pub use self::subtitle::Encoder as SubtitleEncoder;
 
 pub mod motion_estimation;
 pub use self::motion_estimation::MotionEstimation;
