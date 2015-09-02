946fb3501da98082d619218f750b31cd03d6cb20
lummax <luogpg@googlemail.com>
2015-09-02 14:25:52 +0200

    filter/filtergraph: implement `new()` and `drop()`

--- src/filter/filtergraph.rs
+++ src/filter/filtergraph.rs
@@ -1,6 +1,8 @@
+use std::ptr;
 use std::marker::PhantomData;
 
 use ffi::*;
+use ::Error;
 
 pub struct FilterGraph<'a> {
 	ptr: *mut AVFilterGraph,
@@ -29,5 +31,33 @@ impl<'a> FilterGraph<'a> {
 	pub unsafe fn take(mut self) -> *mut AVFilterGraph {
 		self._own = false;
 		self.ptr
+	}
+}
+
+impl<'a> FilterGraph<'a> {
+	pub fn new() -> Result<Self, Error> {
+		let ptr = unsafe {
+			avfilter_graph_alloc()
+		};
+		if ptr.is_null() {
+			Err(Error::Unknown)
+		}
+		else {
+			Ok(FilterGraph {
+				ptr: ptr,
+				_own: true,
+				_marker: PhantomData
+			})
+		}
+	}
+}
+
+impl<'a> Drop for FilterGraph<'a> {
+	fn drop(&mut self) {
+		unsafe {
+			if self._own && self.as_ptr() != ptr::null() {
+				avfilter_graph_free(&mut self.as_mut_ptr() as *mut _);
+			}
+		}
 	}
 }
