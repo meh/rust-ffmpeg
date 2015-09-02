d0d89e44afad7b5bbf85d826ea9c41a71873a8f7
lummax <luogpg@googlemail.com>
2015-09-02 14:21:03 +0200

    filter/filter: implement `get_by_name()`

--- src/filter/filter.rs
+++ src/filter/filter.rs
@@ -1,6 +1,8 @@
+use std::ffi::CString;
 use std::marker::PhantomData;
 
 use ffi::*;
+use ::Error;
 
 pub struct Filter<'a> {
 	ptr: *mut AVFilter,
@@ -29,5 +31,25 @@ impl<'a> Filter<'a> {
 	pub unsafe fn take(mut self) -> *mut AVFilter {
 		self._own = false;
 		self.ptr
+	}
+}
+
+impl<'a> Filter<'a> {
+	pub fn get_by_name(name: &str) -> Result<Self, Error> {
+		let name_cstr = CString::new(name).unwrap();
+		let ptr = unsafe {
+			avfilter_get_by_name(name_cstr.as_ptr())
+		};
+		if !ptr.is_null() {
+			// there seems to be no way to deallocate a `AVFilter`
+			Ok(Filter {
+				ptr: ptr,
+				_own: false,
+				_marker: PhantomData,
+			})
+		}
+		else {
+			Err(Error::InvalidData)
+		}
 	}
 }
