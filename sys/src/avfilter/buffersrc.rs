95d8f31ec76fab4ed76571afedf4c2f77c710313
meh <meh@schizofreni.co>
2015-09-04 16:48:37 +0200

    avfilter/buffersrc: make a pointer to const

--- sys/src/avfilter/buffersrc.rs
+++ sys/src/avfilter/buffersrc.rs
@@ -9,7 +9,7 @@ pub const AV_BUFFERSRC_FLAG_KEEP_REF:        c_int = 8;
 
 extern {
 	pub fn av_buffersrc_add_ref(buffer_src: *mut AVFilterContext, picref: *mut AVFilterBufferRef, flags: c_int) -> c_int;
-	pub fn av_buffersrc_get_nb_failed_requests(buffer_src: *mut AVFilterContext) -> c_uint;
+	pub fn av_buffersrc_get_nb_failed_requests(buffer_src: *const AVFilterContext) -> c_uint;
 	pub fn av_buffersrc_write_frame(ctx: *mut AVFilterContext, frame: *const AVFrame) -> c_int;
 	pub fn av_buffersrc_add_frame(ctx: *mut AVFilterContext, frame: *mut AVFrame) -> c_int;
 	pub fn av_buffersrc_add_frame_flags(buffer_src: *mut AVFilterContext, frame: *mut AVFrame, flags: c_int) -> c_int;
