22ccc04f2d3dc37f1e058eb3fbd8b3753d04e18b
Zhiming Wang <i@zhimingwang.org>
2018-09-14 21:06:56 -0400

    util::color: implement .name() for {Primaries, Range, TransferCharacteristic}

--- src/util/color/range.rs
+++ src/util/color/range.rs
@@ -9,6 +9,18 @@ pub enum Range {
 	JPEG,
 }
 
+impl Range {
+    pub fn name(&self) -> Option<&'static str> {
+        if *self == Range::Unspecified {
+            return None;
+        }
+        unsafe {
+            let ptr = av_color_range_name((*self).into());
+            ptr.as_ref().map(|ptr| from_utf8_unchecked(CStr::from_ptr(ptr).to_bytes()))
+        }
+    }
+}
+
 impl From<AVColorRange> for Range {
 	fn from(value: AVColorRange) -> Self {
 		match value {
