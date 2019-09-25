844eaa11a1f3af10f934ebe4213d10c3cbb1ef45
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-09-25 11:25:15 +0200

    remove deprecated usage of uint64_t

--- sys/src/avutil/util.rs
+++ sys/src/avutil/util.rs
@@ -1,7 +1,7 @@
-use libc::{c_int, int64_t};
+use libc::c_int;
 use {AVRational, AV_TIME_BASE};
 
-pub const AV_NOPTS_VALUE: int64_t = 0x8000000000000000u64 as int64_t;
+pub const AV_NOPTS_VALUE: i64 = 0x8000000000000000u64 as i64;
 pub const AV_TIME_BASE_Q: AVRational = AVRational {
     num: 1,
     den: AV_TIME_BASE as c_int,
