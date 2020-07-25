808a92fb2e84efa2fa319e23149ca610819023c9
Zhiming Wang <i@zhimingwang.org>
2020-07-26 01:01:21 +0800

    Fix clippy::ptr_offset_with_cast

--- src/format/chapter/chapter.rs
+++ src/format/chapter/chapter.rs
@@ -13,9 +13,7 @@ impl<'a> Chapter<'a> {
 	}
 
 	pub unsafe fn as_ptr(&self) -> *const AVChapter {
-		*(*self.context.as_ptr())
-			.chapters
-			.offset(self.index as isize)
+		*(*self.context.as_ptr()).chapters.add(self.index)
 	}
 }
 
