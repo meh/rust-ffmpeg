6862ad3a730070f1b643d5d7a3088e6050aa5824
Mattias Bergström <mattias.bergstrom@outlook.com>
2023-10-12 11:28:34 +0200

    Conditional compile different callbacks

--- src/util/log.rs
+++ src/util/log.rs
@@ -16,7 +16,22 @@ pub fn set_level(level: tracing::Level) {
 	}
 }
 
+#[cfg(target_os = "macos")]
 unsafe extern "C" fn callback(_ptr: *mut c_void, level: c_int, fmt: *const c_char, args: va_list) {
+	let string = vsprintf(fmt, args).unwrap();
+
+	match level {
+		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!(string),
+		AV_LOG_WARNING => tracing::warn!(string),
+		AV_LOG_INFO => tracing::info!(string),
+		AV_LOG_VERBOSE | AV_LOG_DEBUG => tracing::debug!(string),
+		AV_LOG_TRACE => tracing::trace!(string),
+		_ => {}
+	};
+}
+
+#[cfg(not(target_os = "macos"))]
+unsafe extern "C" fn callback(_ptr: *mut c_void, level: c_int, fmt: *const c_char, args: *mut __va_list_tag) {
 	let string = vsprintf(fmt, args).unwrap();
 
 	match level {
