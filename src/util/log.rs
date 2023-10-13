9247e98efaa63fd10d3fbab5b464b3cdd880ff77
Mattias Bergström <mattias.bergstrom@outlook.com>
2023-10-13 16:38:25 +0200

    Trim output

--- src/util/log.rs
+++ src/util/log.rs
@@ -6,7 +6,7 @@ use crate::ffi::*;
 
 #[cfg(target_os = "macos")]
 unsafe extern "C" fn callback(_ptr: *mut c_void, level: c_int, fmt: *const c_char, args: va_list) {
-	let string = vsprintf(fmt, args).unwrap();
+	let string = vsprintf(fmt, args).unwrap().trim();
 
 	match level {
 		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!(string),
@@ -20,7 +20,7 @@ unsafe extern "C" fn callback(_ptr: *mut c_void, level: c_int, fmt: *const c_cha
 
 #[cfg(not(target_os = "macos"))]
 unsafe extern "C" fn callback(_ptr: *mut c_void, level: c_int, fmt: *const c_char, args: *mut __va_list_tag) {
-	let string = vsprintf(fmt, args).unwrap();
+	let string = vsprintf(fmt, args).unwrap().trim();
 
 	match level {
 		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!(string),
