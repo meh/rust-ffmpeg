7d5e03d2969bb76fa7a4462f6af5c9f3d2e358d5
Mattias Bergström <mattias.bergstrom@outlook.com>
2023-11-17 09:56:59 +0100

    Log under the message field instead of string

--- src/util/log.rs
+++ src/util/log.rs
@@ -14,11 +14,11 @@ unsafe extern "C" fn callback(_ptr: *mut c_void, level: c_int, fmt: *const c_cha
 	let string = string.trim();
 
 	match level {
-		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!(string),
-		AV_LOG_WARNING => tracing::warn!(string),
-		AV_LOG_INFO => tracing::info!(string),
-		AV_LOG_VERBOSE | AV_LOG_DEBUG => tracing::debug!(string),
-		AV_LOG_TRACE => tracing::trace!(string),
+		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!("{string}"),
+		AV_LOG_WARNING => tracing::warn!("{string}"),
+		AV_LOG_INFO => tracing::info!("{string}"),
+		AV_LOG_VERBOSE | AV_LOG_DEBUG => tracing::debug!("{string}"),
+		AV_LOG_TRACE => tracing::trace!("{string}"),
 		_ => {}
 	};
 }
@@ -33,11 +33,11 @@ unsafe extern "C" fn callback(_ptr: *mut c_void, level: c_int, fmt: *const c_cha
 	let string = string.trim();
 
 	match level {
-		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!(string),
-		AV_LOG_WARNING => tracing::warn!(string),
-		AV_LOG_INFO => tracing::info!(string),
-		AV_LOG_VERBOSE | AV_LOG_DEBUG => tracing::debug!(string),
-		AV_LOG_TRACE => tracing::trace!(string),
+		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!("{string}"),
+		AV_LOG_WARNING => tracing::warn!("{string}"),
+		AV_LOG_INFO => tracing::info!("{string}"),
+		AV_LOG_VERBOSE | AV_LOG_DEBUG => tracing::debug!("{string}"),
+		AV_LOG_TRACE => tracing::trace!("{string}"),
 		_ => {}
 	};
 }
